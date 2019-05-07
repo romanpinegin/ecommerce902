60f62e1d042022c6d8cc0388e2a4a82a34df4b2a
romanpinegin <roman.pinegin@gmail.com>
2019-05-07 15:17:48 +0300

    Request_2

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/category/{id}", name="category_show")
+     */
+    public function show(Category $category)
+    {
+        return $this->render('category/show.html.twig', [
+            'category' => $category,
+        ]);
+    }
+}
