9ffbff12d3fe31577033a1313e4002dc8cd84c21
romanpinegin <roman.pinegin@gmail.com>
2019-05-12 22:48:25 +0300

    Cart ajax

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -16,8 +16,32 @@ class OrderController extends AbstractController
     public function addToCart(Product $product, OrdersService $ordersService, Request $request)
     {
         $ordersService->addToCart($product);
+
+        if ($request->isXmlHttpRequest()) {
+            return $this->headerCart($ordersService);
+        }
+
         $referer = $request->headers->get('Referer');
 
         return $this->redirect($referer);
+    }
+
+    /**
+     * @Route("/cart", name="order_cart")
+     */
+    public function cart(OrdersService $ordersService)
+    {
+        return $this->render('order/cart.html.twig',
+        [
+            'order' => $ordersService->getOrderFromCart()
+        ]);
+
+    }
+
+    public function headerCart(OrdersService $ordersService)
+    {
+        return $this->render('order/headerCart.html.twig', [
+            'order' => $ordersService->getOrderFromCart(),
+        ]);
     }
 }
---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class AppExtension extends AbstractExtension
+{
+
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('money', [$this, 'formatMoney'])
+        ];
+    }
+
+    public function formatMoney($value)
+    {
+        return twig_localized_currency_filter($value / 100, 'UAH');
+    }
+}
